343105d8a5e4e612ea4af577863647c829f81d06
Samith Wickramasekara <samithwik@gmail.com>
2026-02-08 18:15:51 +0000

    right side of the who we are section needs to be redesigned. Do some typ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,16 @@ import { Contacts } from "@/components/Contacts";
 import { Hero } from "@/components/Hero";
 import { Industries } from "@/components/Industries";
 import { Navbar } from "@/components/Navbar";
-import { Services } from "@/components/Services";
-import { Stats } from "@/components/Stats";
 import { Success } from "@/components/Success";
+import { WhoWeAre } from "@/components/WhoWeAre";
 
 const Page = () => {
   return (
     <main>
       <Navbar />
       <Hero />
-      <Services />
+      <WhoWeAre />
       <Industries />
-      <Stats />
       <Success />
       <Contacts />
     </main>
